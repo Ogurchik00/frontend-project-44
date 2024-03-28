80aa66b3953945bb2b12ba5e299fa06bb8047b37
Nikita <nik.skripka.000@gmail.com>
2024-03-28 21:59:41 +0900

    added brain-calc game

---
+++ bin/brain-calc.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import greetings from '../src/greetings.js';
+
+
+const name = greetings()
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+const operator = ['+', '-', '*']
+
+const calc = (num, numTwo, operator) => {
+  switch(operator) {
+    case '+': 
+      return num + numTwo;
+      break;
+    case '-': 
+      return num - numTwo;
+      break;
+    case '*':
+      return num * numTwo;
+      break;
+  }
+}
+
+const game = () => {
+  for(let i = 0; i <= 3; i += 1) {
+    
+    if(i == 3) {
+      return console.log(`Congratulations, ${name}!`)
+    }
+
+    const randomNum = Math.floor(Math.random() * 99 + 1)
+    const randomNumTwo = Math.floor(Math.random() * 99 + 1)
+    const operation = operator[Math.floor(Math.random() * 2 + 1)]
+    console.log(`Question: ${randomNum} ${operation} ${randomNumTwo}`)
+    const answer = readlineSync.question('Your answer: ');
+
+    if(answer == calc(randomNum, randomNumTwo, operation)) {
+      console.log('Correct!')
+    } else {
+     return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${calc(randomNum, randomNumTwo, operation)}'.
+Let's try again, ${name}!`)
+    }
+  }
+}
+
+game()
